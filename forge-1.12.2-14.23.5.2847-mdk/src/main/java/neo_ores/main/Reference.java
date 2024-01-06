75b5ae7e326a12b6d650dab63e3a199f00307896
Quark0257 <57037206+Quark0257@users.noreply.github.com>
2024-01-06 22:48:30 +0900

    Update Reference.java

--- forge-1.12.2-14.23.5.2847-mdk/src/main/java/neo_ores/main/Reference.java
+++ forge-1.12.2-14.23.5.2847-mdk/src/main/java/neo_ores/main/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
 	public static final String MOD_ID = "neo_ores";
 	public static final String MOD_NAME = "Neo Ores II";
-	public static final String MOD_VERSION = "1.3.19-alpha";
+	public static final String MOD_VERSION = "1.4.0-alpha";
 	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12]";
 
 	public static final String CLIENT_PROXY_CLASS = "neo_ores.proxy.ClientProxy";
